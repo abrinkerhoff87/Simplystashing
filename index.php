fedd1da69fa79051b894ebe32e76acbb1503baf5
Andrew Brinkerhoff <andrew.brinkerhoff@outlook.com>
2015-06-27 16:19:20 -0400

    Added index.php, header.html

---
+++ index.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+ 	<head>
+		<title>Test</title>
+		<link href="css/main_style.css" type="css/style" rel="stylesheet">
+		<link href="css/header_style.css" type="css/style" rel="stylesheet">
+    </head>
+    <body>
+    	<?php include 'header.html'; ?>
+		<div class="sandbox">
+			<?php echo "string"; ?>
+	  	</div>
+  	</body>
+</html>
