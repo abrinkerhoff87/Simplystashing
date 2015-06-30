4f988b0a503af35566be39edf4916555ff187680
Andrew Brinkerhoff <andrew.brinkerhoff@outlook.com>
2015-06-29 22:32:03 -0400

    finished CSS on navbar.html

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 		<title>Test</title>
 		<link href="css/main_style.css" type="text/css" rel="stylesheet">
 		<link href="css/header_style.css" type="text/css" rel="stylesheet">
+		<link href="css/navbar.css" type="text/css" rel="stylesheet">
     </head>
     <body>
     	<?php include 'header.html'; ?>
     	<?php include 'navbar.html'; ?>
 		<div class="sandbox">
-			dfasfda
+			<p>dasfads</p>
 	  	</div>
   	</body>
 </html>
