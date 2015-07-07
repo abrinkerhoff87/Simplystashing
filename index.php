4a1cfcb827ef0827ad0e48eb6ae4adad451a4902
Andrew Brinkerhoff <andrew.brinkerhoff@outlook.com>
2015-07-07 00:02:20 -0400

    started footer

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<link href="css/main_style.css" type="text/css" rel="stylesheet">
 		<link href="css/header_style.css" type="text/css" rel="stylesheet">
 		<link href="css/navbar.css" type="text/css" rel="stylesheet">
+        <link href="css/footer_style.css" type="text/css" rel="stylesheet">
     </head>
     <body>
     	<?php include 'header.html'; ?>
@@ -12,5 +13,6 @@
 		<div class="sandbox">
 			<p>dasfadsdsafafadf</p>
 	  	</div>
+        <?php include 'footer.html'; ?>
   	</body>
 </html>
