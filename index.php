289a5f16368b063b12016f967de4cea1cf261e7a
Andrew Brinkerhoff <andrew.brinkerhoff@outlook.com>
2015-07-07 21:32:54 -0400

    finished footer, started and finished slide show

--- index.php
+++ index.php
@@ -6,12 +6,38 @@
 		<link href="css/header_style.css" type="text/css" rel="stylesheet">
 		<link href="css/navbar.css" type="text/css" rel="stylesheet">
         <link href="css/footer_style.css" type="text/css" rel="stylesheet">
+        
+        
+        <script src="slideShow.js"></script>
+        
+        
     </head>
     <body>
     	<?php include 'header.html'; ?>
     	<?php include 'navbar.html'; ?>
 		<div class="sandbox">
-			<p>dasfadsdsafafadf</p>
+			<div id="slideShowImages">
+                <img src="pics/100_0835.jpg" alt="Slide 1" />
+                <img src="pics/100_0836.jpg" alt="Slide 2" />
+                <img src="pics/100_0837.jpg" alt="Slide 3" />
+                <img src="pics/100_0964.jpg" alt="Slide 4" />
+                <img src="pics/100_0965.jpg" alt="Slide 5" />
+                <img src="pics/100_1048.jpg" alt="Slide 6" />
+                <img src="pics/fat quaters.jpg" alt="Slide 7" />
+                <img src="pics/imag0061-crop-u11733.jpg" alt="Slide 8" />
+                <img src="pics/imag0064-crop-u11742.jpg" alt="Slide 9" />
+                <img src="pics/imag0066-crop-u11819.jpg" alt="Slide 10" />
+                <img src="pics/imag0067-crop-u11760.jpg" alt="Slide 11" />
+                <img src="pics/imag0070-crop-u11769.jpg" alt="Slide 12" />
+                <img src="pics/imag0072-crop-u11778.jpg" alt="Slide 13" />
+                <img src="pics/imag0075-crop-u11787.jpg" alt="Slide 14" />
+                <img src="pics/imag0076-crop-u11796.jpg" alt="Slide 15" />
+                <img src="pics/imag0078-crop-u11805.jpg" alt="Slide 16" />
+                <img src="pics/notions2.jpg" alt="Slide 17" />
+                <img src="pics/patterns2.jpg" alt="Slide 18" />
+                <img src="pics/purs acce.jpg" alt="Slide 19" />
+                <img src="pics/thered2.jpg" alt="Slide 20" />
+            </div>
 	  	</div>
         <?php include 'footer.html'; ?>
   	</body>
