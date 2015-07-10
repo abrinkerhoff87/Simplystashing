7ac901fa5172e1b0d999e947ce1f56339af8c9c0
Andrew Brinkerhoff <andrew.brinkerhoff@outlook.com>
2015-07-09 21:45:09 -0400

    added pages

---
+++ index - Copy.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+<html>
+ 	<head>
+		<title>Home</title>
+		<link href="css/main_style.css" type="text/css" rel="stylesheet">
+		<link href="css/header_style.css" type="text/css" rel="stylesheet">
+		<link href="css/navbar.css" type="text/css" rel="stylesheet">
+        <link href="css/footer_style.css" type="text/css" rel="stylesheet">
+        <script src="scripts/slideShow.js"></script>       
+    </head>
+    <body>
+    	<?php include 'header.html'; ?>
+    	<?php include 'navbar.html'; ?>
+		<div class="sandbox">
+			
+	  	</div>
+        <?php include 'footer.html'; ?>
+  	</body>
+</html>
