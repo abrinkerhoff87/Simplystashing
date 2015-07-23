470fed73fdf99bb8e48bb1af0c4429380757f93e
Andrew Brinkerhoff <andrew.brinkerhoff@outlook.com>
2015-07-23 00:22:19 -0400

    updat

--- abouts_us.php
+++ abouts_us.php
@@ -1,10 +1,11 @@
 <!Doctype html>
 <html>
  	<head>
-		<title>Home</title>
+        <title>About Us</title>
 		<link href="css/main_style.css" type="text/css" rel="stylesheet">
 		<link href="css/header_style.css" type="text/css" rel="stylesheet">
 		<link href="css/navbar.css" type="text/css" rel="stylesheet">
+        <link href="css/about_us.css" type="text/css" rel="stylesheet">
         <link href="css/footer_style.css" type="text/css" rel="stylesheet">
         <script src="slideShow.js"></script>       
     </head>
@@ -12,7 +13,12 @@
     	<?php include 'header.html'; ?>
     	<?php include 'navbar.html'; ?>
 		<div class="sandbox">
-			
+			<div>
+                <p id="about_us_header">We are a full service quilt shop offering many top name brands of 1st quality fabric including:</p>
+            </div>
+            <div class="header_bottom">
+            
+            </div>
 	  	</div>
         <?php include 'footer.html'; ?>
   	</body>
